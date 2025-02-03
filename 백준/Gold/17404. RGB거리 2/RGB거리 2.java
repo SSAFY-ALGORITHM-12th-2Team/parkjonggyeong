00e2f3afa1fc0cc1b4024d85d32a8bb42e00d795
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2025-02-03 19:00:08 +0900

    Update RGB거리 2.java

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -5,8 +5,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     static final int INF = 987654321;
-    
-      public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[][] rgbs = new int[n][3];
@@ -22,29 +22,25 @@ public class Main {
         System.out.println(solution(n, rgbs));
         br.close();
     }
-    
+
     public static int solution(int n, int[][] rgbs) {
         int answer = INF;
-        int[][] dp = new int[n][3];
 
         for (int initialColor = 0; initialColor < 3; initialColor++) {
+            int[][] dp = new int[n][3];
+
             for (int color = 0; color < 3; color++) {
-                dp[0][color] = (initialColor == color) ? rgbs[0][color] : INF;
+                dp[0][color] = (color == initialColor) ? rgbs[0][color] : INF;
             }
 
             for (int house = 1; house < n; house++) {
-                for (int currentColor = 0; currentColor < 3; currentColor++) {
-                    dp[house][currentColor] = INF;
-                    for (int prevColor = 0; prevColor < 3; prevColor++) {
-                        if (currentColor != prevColor) {
-                            dp[house][currentColor] = Math.min(dp[house][currentColor], dp[house - 1][prevColor] + rgbs[house][currentColor]);
-                        }
-                    }
-                }
+                dp[house][0] = Math.min(dp[house - 1][1], dp[house - 1][2]) + rgbs[house][0];
+                dp[house][1] = Math.min(dp[house - 1][0], dp[house - 1][2]) + rgbs[house][1];
+                dp[house][2] = Math.min(dp[house - 1][0], dp[house - 1][1]) + rgbs[house][2];
             }
 
             for (int lastColor = 0; lastColor < 3; lastColor++) {
-                if (initialColor != lastColor) {
+                if (lastColor != initialColor) {
                     answer = Math.min(answer, dp[n - 1][lastColor]);
                 }
             }
